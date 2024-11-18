88fa85c4da424ade29d92d8c4191b55e170e8007
Giuseppe <giuseppe.gautieri00@gmail.com>
2024-11-18 16:23:35 +0100

    Add files via upload

--- DNA_translation/DNA_translation.py
+++ DNA_translation/DNA_translation.py
@@ -1,29 +1,28 @@
 from Bio.Seq import Seq
 from Bio import SeqIO
 
+class DNA_sequence():
+    def __init__(self, sequence):
+        """ Load sequences from a list of FASTA files."""
+        for record in SeqIO.parse(fasta_file, "fasta"):
+            self.sequence = str(record.seq)
+        if len(self.sequence) == 0:
+            raise ValueError(f"No sequences found in {fasta_file}.")
 
-def load_sequence(fasta_file):
-    """ Load sequences from a list of FASTA files."""
-    for record in SeqIO.parse(fasta_file, "fasta"):
-        sequence = str(record.seq)
-    if len(sequence) == 0:
-        raise ValueError(f"No sequences found in {fasta_file}.")
-    return sequence
-
-def translate(sequence):
-    # translate sequence in aminoacids
-    if len(sequence) % 3 == 0 :
-        protein_sequence = Seq.translate(sequence, "Standard")
-    elif (len(sequence) - 1) % 3 == 0:
-        protein_sequence = Seq.translate(sequence[:-1], "Standard")
-    else:
-        protein_sequence = Seq.translate(sequence[:-2], "Standard")
-    return protein_sequence
+    def translate(self):
+        """Translate the nucleotide sequence in amino acids sequence using codon table"""
+        if len(self.sequence) % 3 == 0 :
+            protein_sequence = Seq.translate(Seq(self.sequence), "Standard")
+        elif len(self.sequence) - 1 % 3 == 0:
+            protein_sequence = Seq.translate(Seq(self.sequence)[:-1], "Standard")
+        else:
+            protein_sequence = Seq.translate(Seq(self.sequence)[:-2], "Standard")
+        return protein_sequence
 
 
 fasta_file = "" # add file path
-sequence = load_sequence(fasta_file)
-protein_sequence = translate(sequence)
+sequence = DNA_sequence(fasta_file)
+protein_sequence = sequence.translate()
 print(f"The sequence is {protein_sequence}")
 
 
